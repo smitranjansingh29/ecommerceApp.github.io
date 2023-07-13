8a604952e2545ebac3b3b045d855043f064cc340
smitranjansingh29 <smitpa2918@gmail.com>
2023-07-13 12:18:54 +0530

    Final project1

---
+++ src/utils/appSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+const appSlice = createSlice(
+    {
+        name:"app",
+        initialState:{
+            products: [],
+            cart: [],
+            itemToDisplay: "",
+            totalCart: 0,
+        },
+        reducers:{
+            Add_products:(state,actions)=>{
+                
+
+            }
+        }
+    }
+)
+export default appSlice;
